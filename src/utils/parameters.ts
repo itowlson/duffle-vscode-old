81416f5f4ea341b6393d6f5dfab790f8a3a48d07
Ivan Towlson <itowlson@microsoft.com>
2018-11-19 14:12:44 +1300

    The parameter validator of screaming terror

--- src/utils/parameters.ts
+++ src/utils/parameters.ts
@@ -9,15 +9,18 @@ interface ParameterDefinitionMapping extends ParameterDefinition {
     readonly name: string;
 }
 
+const PARAMETER_FORM_ID = 'pvform';
+
 export async function promptForParameters(bundlePick: BundleSelection, bundleManifest: BundleManifest, actionName: string, prompt: string): Promise<ParameterValuesPromptResult> {
     const definitions = parseParameters(bundleManifest);
     if (!definitions || definitions.length === 0) {
         return { cancelled: false, value: {} };
     }
 
-    const parameterFormId = 'pvform';
+    const parameterFormId = PARAMETER_FORM_ID;
 
     const html = `<h1>${prompt}</h1>
+    ${validationScript(definitions)}
     <form id='${parameterFormId}'>
     ${parameterEntryTable(definitions)}
     </form>
@@ -56,7 +59,68 @@ function inputWidget(p: ParameterDefinitionMapping): string {
         return `<select name="${p.name}">${opts}</select>`;
     }
     const defval = p.defaultValue ? `${p.defaultValue}` : '';
-    return `<input name="${p.name}" type="text" value="${defval}" />`;
+    return `<input name="${p.name}" type="text" value="${defval}" /><span id="${p.name}_validation_display" />`;
+}
+
+function validationScript(ps: ParameterDefinitionMapping[]): string {
+    return `<script>
+window.addEventListener('input', function(e) { validate(); }, false);
+function validate() {
+    ${validationScriptLines(ps)}
+}
+</script>`;
+}
+
+function validationScriptLines(ps: ParameterDefinitionMapping[]): string {
+    return `for (const e of document.forms['${PARAMETER_FORM_ID}'].elements) {
+        ${ps.map(validationScriptLine).join('\n')};
+    }`;
+}
+
+function validationScriptLine(p: ParameterDefinitionMapping): string {
+    const body = validationCode(p);
+    if (body && body.length > 0) {
+        return `if (e.name === '${p.name}') {
+            ${body}
+        }`;
+    }
+    return '';
+}
+
+function validationCode(p: ParameterDefinitionMapping): string | undefined {
+    if (p.type === "bool" || p.allowedValues) {
+        return undefined;
+    }
+    const validationElementID = `${p.name}_validation_display`;
+    const validationMessageProperty = `document.getElementById('${validationElementID}').innerText`;
+    if (p.type === "int") {
+        let code = `
+        ${validationMessageProperty} = '';
+        nval = Number.parseInt(e.value);
+        if (isNaN(nval)) { ${validationMessageProperty} = 'Must be a number'; }
+        `;
+        if (p.minValue !== undefined) {
+            code += `if (nval < ${p.minValue}) { ${validationMessageProperty} = 'Must be at least ${p.minValue}'; }
+            `;
+        }
+        if (p.maxValue !== undefined) {
+            code += `if (nval > ${p.maxValue}) { ${validationMessageProperty} = 'Must be at most ${p.maxValue}'; }
+            `;
+        }
+        return code;
+    }
+    let code = `
+    ${validationMessageProperty} = '';
+    `;
+    if (p.minLength !== undefined) {
+        code += `if (e.value.length < ${p.minLength}) { ${validationMessageProperty} = 'Must be at least ${p.minLength} characters'; }
+        `;
+    }
+    if (p.maxLength !== undefined) {
+        code += `if (e.value.length > ${p.maxLength}) { ${validationMessageProperty} = 'Must be at most ${p.maxLength} characters'; }
+        `;
+    }
+    return code;
 }
 
 function parseParameters(manifest: BundleManifest): ParameterDefinitionMapping[] {
